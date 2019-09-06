f902e2cb007d666d34c4e7b951af110a90e12694
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-06 11:10:22 -0500

    Making and axios call and displaying data to the screen using lifecycle method

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import NavBar from './components/Navbar'
+import axios from 'axios';
 
+// Components
+import NavBar from './components/Navbar'
+import PlayerCard from './components/PlayerCard'
+// Styles
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-			<NavBar/>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		data: []
+	}
+
+	componentDidMount() {
+		axios('http://localhost:5000/api/players')
+		.then(res => this.setState({data: res.data}))
+	}
+	render(){
+			return (
+			<div className="App">
+				<NavBar/>
+				<div className="player-container">
+					{this.state.data.map(player => {
+						return <PlayerCard key={player.id} player={player}/>
+					})}
+				</div>
+			</div>
+		);
+	}
+  
 }
 
 export default App;
