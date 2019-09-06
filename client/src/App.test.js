b69b4fea77aa3594d6144d5154a6496d18fb13de
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-06 11:35:34 -0500

    Completed base testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from "@testing-library/react";
 import App from './App';
+import Navbar from './components/Navbar';
 
+// Testing that App component renders to the UI
 test('App renders without crashing', () => {
 	render(<App />);
 });
+
+// Testing that Navbar component renders to the UI
+test('Navbar renders without crashing', () => {
+	render(<Navbar />);
+});
+
